11b8559f4c856cbafe88e2e8a6cfc453c007e7d0
wolf <oscarprograb@gmail.com>
2020-06-15 23:32:02 -0500

    xd

--- ejercicio 1/cliente.py
+++ ejercicio 1/cliente.py
@@ -1,55 +1,32 @@
 import socket
 import pyaudio
 import wave
+from scipy.io import wavfile
+import sounddevice
 
 
-class voz:
-    def __init__(self):
-        chunk = 1024  # Record in chunks of 1024 samples
-        sample_format = pyaudio.paInt16  # 16 bits per sample
-        channels = 2
-        fs = 44100  # Record at 44100 samples per second
-        seconds = 3
-        filename = "output.wav"
-        p = pyaudio.PyAudio()  # Create an interface to PortAudio
+class Grabadora:
+    def __init__(self, fs=44100, canales=64, dtipo='float64'):
+        self.fs = fs
+        print(fs)
+        #sounddevice.default.device = 8
+        sounddevice.default.samplerate = fs
+        sounddevice.default.channels = 1
+        sounddevice.default.dtype = dtipo
 
-    def grabar(self):
-        print('Recording')
-
-        stream = self.p.open(format=self.sample_format,
-                             channels=self.channels,
-                             rate=self.fs,
-                             frames_per_buffer=self.chunk,
-                             input=True)
-
-        frames = []  # Initialize array to store frames
-        # Store data in chunks for 3 seconds
-        for i in range(0, int(self.fs / self.chunk * self.seconds)):
-            data = stream.read(self.chunk)
-            frames.append(data)
-
-        # Stop and close the stream
-        stream.stop_stream()
-        stream.close()
-        # Terminate the PortAudio interface
-        self.p.terminate()
-
-        print('Finished recording')
-
-        # Save the recorded data as a WAV file
-        wf = wave.open(self.filename, 'wb')
-        wf.setnchannels(self.channels)
-        wf.setsampwidth(self.p.get_sample_size(self.sample_format))
-        wf.setframerate(self.fs)
-        wf.writeframes(b''.join(frames))
-        wf.close()
+    def grabar(self, duracion=3, zoom=10):
+        print("Grabando...")
+        muestras = sounddevice.rec(int(duracion * self.fs))
+        sounddevice.wait()
+        print("Listo!!")
+        wavfile.write("output.wav", self.fs, muestras)
 
 
 # -------------------------------------------
-HOST = "8.40.1.237"  # The server's hostname or IP address
-PORT = 1234  # The port used by the server
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
 buffer_size = 1024
-vozobj = voz()
+grabadora = Grabadora()
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPClientSocket:
     TCPClientSocket.connect((HOST, PORT))
     print("Conexion establecida...")
@@ -68,8 +45,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPClientSocket:
         # ---------------------------voz-*-------------------------
         # if es_turno:
         if True:
-            vozobj.grabar()
+            grabadora.grabar()
             voz = open("output.wav", "rb")
             SD = voz.read()
             TCPClientSocket.sendall(SD)
+
+            break
         # ---------------------------------------------------------
--- ejercicio 1/ejercicio1.py
+++ ejercicio 1/ejercicio1.py
@@ -9,7 +9,7 @@ from ibm_watson import SpeechToTextV1
 from ibm_watson.websocket import RecognizeCallback, AudioSource
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-from Server import gestion_conexiones
+# from Server import gestion_conexiones
 
 authenticator = IAMAuthenticator('HuuOkm4voT_3pceZje2oHxhXIZWMX6HepdKWUibJux3H')
 speech_to_text = SpeechToTextV1(
@@ -44,10 +44,12 @@ def traducirVozaTexto():
             content_type='audio/wav',
             recognize_callback=myRecognizeCallback,
             model='es-MX_NarrowbandModel',
-            keywords=['ojos', 'cabello', 'sexo', "estatura", "piel", "nombre"],
+            keywords=["tiene"],
             keywords_threshold=0.5,
             max_alternatives=1)
 
+
+        print(speech_to_text.get_model(0))
 
 class AdivinaQuien:
     colores_de_ojos = ["castaño", "ámbar", "avellana", "verde", "azul", "gris"]
@@ -193,75 +195,80 @@ class AdivinaQuien:
             print("-------------------------------------")
 
 
-class Servidor(object):
-    buffer_size = 1024
-    tB = []
-    perdio = False
+buffer_size = 1024
 
-    def servirPorSiempre(self, socketTcp, listaconexiones):
-        try:
-            while True:
-                client_conn, client_addr = socketTcp.accept()
-                print("Conectado a", client_addr)
-                listaconexiones.append(client_conn)
-                thread_read = threading.Thread(target=self.recibir_datos, args=[client_conn, client_addr])
-                thread_read.start()
-                gestion_conexiones(self.listaConexiones)
-        except Exception as e:
-            print(e)
 
-    def gestion_conexiones(listaconexiones):
-        for conn in listaconexiones:
-            if conn.fileno() == -1:
-                listaconexiones.remove(conn)
-        print("hilos activos:", threading.active_count())
-        print("enum", threading.enumerate())
-        print("conexiones: ", len(listaconexiones))
-        print(listaconexiones)
+def servirPorSiempre(socketTcp, listaconexiones):
+    try:
+        while True:
+            client_conn, client_addr = socketTcp.accept()
+            print("Conectado a", client_addr)
+            listaconexiones.append(client_conn)
+            thread_read = threading.Thread(target=recibir_datos, args=[client_conn, client_addr])
+            thread_read.start()
+            gestion_conexiones(listaConexiones)
+    except Exception as e:
+        print(e)
 
-    def recibir_datos(self, client_conn, client_addr):
-        try:
-            cur_thread = threading.current_thread()
-            print("Recibiendo datos del cliente {} en el {}".format(client_addr, cur_thread.name))
-            # -------------------------------------------------------------------------------------------------------------
-            while True:
-                # archivo de voz
-                voz = open("salser.wav", "wb")
-                recdata = client_conn.recv(self.buffer_size)
+
+def gestion_conexiones(listaconexiones):
+    for conn in listaconexiones:
+        if conn.fileno() == -1:
+            listaconexiones.remove(conn)
+    print("hilos activos:", threading.active_count())
+    print("enum", threading.enumerate())
+    print("conexiones: ", len(listaconexiones))
+    print(listaconexiones)
+
+
+def recibir_datos(client_conn, client_addr):
+    try:
+        cur_thread = threading.current_thread()
+        print("Recibiendo datos del cliente {} en el {}".format(client_addr, cur_thread.name))
+        # -------------------------------------------------------------------------------------------------------------
+        while True:
+            # archivo de voz
+            voz = open("salser.wav", "wb")
+            recdata = client_conn.recv(buffer_size)
+            while recdata:
                 voz.write(recdata)
-                voz.close()
+                recdata = client_conn.recv(buffer_size)
+            print("recdata es")
+            print(recdata)
 
-                # ibm tts
+            voz.close()
+            traducirVozaTexto()
+            input()
+            # ibm tts
 
-                # data = client_conn.recv(buffer_size) #recibe
-                # men = ""
-                # client_conn.sendall(men.encode('utf-8') #envia men
-                # print("Esperando a recibir datos...")
+            # data = client_conn.recv(buffer_size) #recibe
+            # men = ""
+            # client_conn.sendall(men.encode('utf-8') #envia men
+            # print("Esperando a recibir datos...")
 
-            # -------------------------------------------------------------------------------------------------------------
+        # -------------------------------------------------------------------------------------------------------------
 
-
-        except Exception as e:
-            print(e)
-        finally:
-            client_conn.close()
-
-    listaConexiones = []
-    host, port, numConn = sys.argv[1:4]
-
-    if len(sys.argv) != 4:
-        print("usage:", sys.argv[0], "<ip host> <puerto> <numero de conexiones>")
-        sys.exit(1)
-
-    serveraddr = (host, int(port))
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPServerSocket:
-        # TCPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        TCPServerSocket.bind(serveraddr)
-        TCPServerSocket.listen(int(numConn))
-        print("El servidor TCP está disponible y en espera de solicitudes")
-
-        servirPorSiempre(TCPServerSocket, listaConexiones)
+    except Exception as e:
+        print(e)
+    finally:
+        client_conn.close()
 
 
-Servidor = Servidor()
+listaConexiones = []
+# host, port, numConn = sys.argv[1:4]
+host = "127.0.0.1"
+port = "65432"
+numConn = "1"
+# if len(sys.argv) != 4:
+#    print("usage:", sys.argv[0], "<host> <port> <num_connections>")
+#    sys.exit(1)
+
+serveraddr = (host, int(port))
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPServerSocket:
+    # TCPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    TCPServerSocket.bind(serveraddr)
+    TCPServerSocket.listen(int(numConn))
+    print("El servidor TCP está disponible y en espera de " + numConn + " solicitudes")
+
+    servirPorSiempre(TCPServerSocket, listaConexiones)
